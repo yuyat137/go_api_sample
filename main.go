5614f2f9805c3cd69e3a4d065d00d7627da1c6bf
shushikata <shu.42111227@gmail.com>
2023-09-09 22:59:21 +0900

    test

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  _ "github.com/go-sql-driver/mysql" //本コードでmysqlを使う記述が無いが、インポートしたいものに対しては"_"を頭につける決まり。
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql" //本コードでmysqlを使う記述が無いが、インポートしたいものに対しては"_"を頭につける決まり。
 )
 
 func setupRouter() *gin.Engine {
@@ -19,3 +19,4 @@ func main() {
   router := setupRouter()
   router.Run(":8080")
 }
+
